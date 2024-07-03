44358e1b54ca2199d4fb7e973a2829abcf45f38d
Michael Quach <mquac089@uottawa.ca>
2024-07-03 11:08:41 -0400

    test

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>0</h1>
+            <div className="links">
+                <a href="/home">Home</a>
+                <a href="/info">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+            </div>
+        </nav>
+    );
+}
+ 
+
+export default Navbar;
