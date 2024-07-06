a6f43c7e10258b53b08a7aa00c9aa7fa1dcd5587
Michael Quach <mquac089@uottawa.ca>
2024-07-06 16:48:28 -0400

    updated the calandar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-date-picker';
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
 
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 const Calendar = () => {
-    const [selectedDate, setSelectedDate] = useState(null);
+    
     const [friendsList] = useState(['Big Boss', 'Kazuhira Miller', 'Ocelot']);
     const [invitedFriends, setInvitedFriends] = useState([]);
 
@@ -22,7 +18,8 @@ const Calendar = () => {
     return (
         <Container>
             <h2 className='friend-header'>Invite Friends to Paintball</h2>
-            <DatePicker className="schedule" selected={selectedDate} onChange={(date) => setSelectedDate(date)}/>
+            <h4 className="friend-header">Schedule a time</h4>   
+            <input className='check-input-right' type="datetime-local" id="indDate" name="indDate"/>
             <h3 className='friend-header'>Select Friends to Invite</h3>
             <ListGroup>
                 {friendsList.map((friend, index) => (
